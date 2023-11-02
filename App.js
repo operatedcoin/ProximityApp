d98236a7c510af1f031d3f1ed2ad9a535e8bfa4d
Nick <nck@operatedcoin.com>
2023-11-03 09:12:18 +1100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import useBLE from './path_to_useBLE_hook';
 
-export default function App() {
+function App() {
+  const {
+    deviceDistances,
+    scanForPeripherals,
+    stopScanning,
+    requestPermissions
+  } = useBLE();
+
+  useEffect(() => {
+    requestPermissions((granted) => {
+      if (granted) {
+        scanForPeripherals();
+      } else {
+        console.warn("Permissions not granted");
+      }
+    });
+  }, [requestPermissions, scanForPeripherals]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {Object.entries(deviceDistances).map(([deviceName, rssi]) => (
+        <Text key={deviceName}>
+          {deviceName}: RSSI = {rssi}
+        </Text>
+      ))}
+      <Button title="Start Scanning" onPress={scanForPeripherals} />
+      <Button title="Stop Scanning" onPress={stopScanning} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
